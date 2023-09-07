8ea1209a8a4280b32f5a3a772103ec7571162134
faruqmaulana <faruqchodot21@gmail.com>
2023-09-08 02:01:22 +0700

    fix: error build ts eslint

--- src/common/transforms/transformActiviryLog.ts
+++ src/common/transforms/transformActiviryLog.ts
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { type KejuaraanByIdType } from "~/server/api/module/pengajuan/prestasi";
-import { type StepperVerticalProp } from "../components/ui/stepper/StepperVertical";
 import { changeDateFormat } from "../helpers/changeDateFormat";
 
 export const transformActivityLog = (
@@ -7,10 +7,11 @@ export const transformActivityLog = (
 ) => {
   return data?.map((val) => {
     return {
+      id: val.id,
       status: val.status,
-      userName: val.User.name as string,
+      userName: val.User.name,
       catatan: val.catatan,
       date: changeDateFormat(val.createdAt),
     };
-  }) as StepperVerticalProp[];
+  });
 };
