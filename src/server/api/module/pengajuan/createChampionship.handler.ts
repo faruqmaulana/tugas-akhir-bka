fa89f8e99c1ddc758cd5d312925599500b078d8e
faruqmaulana <faruqchodot21@gmail.com>
2023-09-16 11:56:04 +0700

    feat: refactoring championship api handler

---
+++ src/server/api/module/pengajuan/createChampionship.handler.ts
@@ -0,0 +1,134 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { pengajuanPrestasiForm } from "~/common/schemas/module/pengajuan/pengajuan-prestasi.shema";
+import { protectedProcedure } from "../../trpc";
+import { handleDocumentMetaToJSON } from "~/common/libs/handle-document-data";
+import {
+  MOUDLE_KEJUARAAN,
+  PENGAJUAN_MESSAGE_BY_ADMIN_SIDE,
+  PENGAJUAN_MESSAGE_BY_USER_SIDE,
+} from "~/common/constants/MESSAGE";
+import { STATUS } from "~/common/enums/STATUS";
+import { ADD_PRESTASI_LOMBA_SUCCESS } from "~/common/message";
+import { type SuccessPengajuanOnUsersType } from "./prestasi";
+
+const createChampionshipHandler = protectedProcedure
+  .input(pengajuanPrestasiForm)
+  .mutation(async ({ ctx, input }) => {
+    try {
+      const {
+        // THESE LAMPIRAN PAYLOAD
+        dokumenPendukung,
+        fotoPenyerahanPiala,
+        piagamPenghargaan,
+        undanganKejuaraan,
+
+        // PRESTASI DATA TABLE PAYLOAD
+        kegiatan,
+        tanggalKegiatan,
+        penyelenggara,
+        dosenId,
+        orkemId,
+        tingkatKejuaraanId,
+        tingkatPrestasiId,
+
+        // PENGAJUAN ON USER PAYLOAD
+        users,
+        currentUserName,
+      } = input;
+
+      // ** CREATE A LAMPIRAN DATA FIRST TO GET A LAMPIRAN ID
+      const createLampiran = await ctx.prisma.lampiranData.create({
+        data: handleDocumentMetaToJSON({
+          dokumenPendukung,
+          fotoPenyerahanPiala,
+          piagamPenghargaan,
+          undanganKejuaraan,
+        }),
+      });
+
+      // ** CREATE NEW PRESTASI DATA TABLE AND GET THE DATA ID
+      const createPrestasiDataTable = await ctx.prisma.prestasiDataTable.create(
+        {
+          data: {
+            kegiatan,
+            tanggalKegiatan,
+            penyelenggara,
+            dosenId,
+            orkemId,
+            tingkatKejuaraanId,
+            tingkatPrestasiId,
+            lampiranId: createLampiran.id,
+            createdById: ctx.session.user.userId,
+          },
+        }
+      );
+
+      // ** CREATE NEW ENTITY PENGAJUAN ON USERS BASED ON PRETASI DATA TABLE AND USERS
+      const createPengajuanOnUsers =
+        await ctx.prisma.pengajuanOnUsers.createMany({
+          data: users.map((val) => {
+            return {
+              dosenId,
+              userId: val.value,
+              keterangan: val.isKetua ? "Ketua Tim" : "Anggota",
+              prestasiDataTableId: createPrestasiDataTable.id,
+            };
+          }),
+        });
+
+      //** ADD NOTIFICATION MESSAGE */
+      const notificationMessage = await ctx.prisma.notifMessage.create({
+        data: {
+          module: "kejuaraan",
+          status: STATUS.PROCESSED,
+          description: `Pengajuan Prestasi - ${createPrestasiDataTable.kegiatan}`,
+          moduleId: createPrestasiDataTable.id,
+          actionByMahasiswaId: ctx.session.user.userId,
+          forUserMessage: PENGAJUAN_MESSAGE_BY_USER_SIDE(MOUDLE_KEJUARAAN),
+          forAdminMessage: PENGAJUAN_MESSAGE_BY_ADMIN_SIDE(
+            currentUserName,
+            MOUDLE_KEJUARAAN
+          ),
+          userInfo: users,
+        },
+      });
+
+      //** ADD NOTIFICATION IN ADMIN */
+      const admin = await ctx.prisma.user.findMany({
+        where: {
+          role: "ADMIN",
+        },
+      });
+
+      const mergeusers = [...admin, ...users];
+
+      //** ADD ACTIVITY LOG */
+      const createActivityLog = await ctx.prisma.activityLog.create({
+        data: {
+          prestasiDataTableId: createPrestasiDataTable.id,
+          userId: ctx.session.user.userId,
+          status: STATUS.PROCESSED,
+        },
+      });
+
+      //** ADD NOTIFICATION IN RELATED USERS AND ADMINS */
+      await ctx.prisma.notification.createMany({
+        data: mergeusers.map((val: { value?: string; id?: string }) => {
+          return {
+            notificationMessageId: notificationMessage.id,
+            userId: (val.value || val.id) as string,
+            activityLogId: createActivityLog.id,
+          };
+        }),
+      });
+
+      return {
+        message: ADD_PRESTASI_LOMBA_SUCCESS,
+        data: createPengajuanOnUsers,
+      } as SuccessPengajuanOnUsersType;
+    } catch (error) {
+      throw error;
+    }
+  });
+
+export default createChampionshipHandler;
---
+++ src/server/api/module/pengajuan/getAllChampionship.handler.ts
@@ -0,0 +1,67 @@
+import { changeDateFormat } from "~/common/helpers/changeDateFormat";
+import { protectedProcedure } from "../../trpc";
+
+const getAllChampionshipHandler = protectedProcedure.query(async ({ ctx }) => {
+  try {
+    const prestasiData = await ctx.prisma.pengajuanOnUsers.findMany({
+      where: {
+        NOT: { prestasiDataTableId: null },
+      },
+      orderBy: {
+        PrestasiDataTable: { updatedAt: "desc" },
+      },
+      include: {
+        PrestasiDataTable: {
+          include: {
+            lampiran: true,
+            orkem: true,
+            tingkatKejuaraan: true,
+            tingkatPrestasi: true,
+            activityLog: true,
+          },
+        },
+        dosen: { select: { name: true, nidn: true } },
+        user: { select: { name: true, prodi: true } },
+      },
+    });
+
+    const transformedPrestasiData = prestasiData.map((data) => ({
+      id: data.PrestasiDataTable?.id || "-",
+      nama: data.user.name || "-",
+      noSK: data.PrestasiDataTable?.noSK || "-",
+      tanggalSK: changeDateFormat(data.PrestasiDataTable?.tanggalSK),
+      kegiatan: data.PrestasiDataTable?.kegiatan || "-",
+      tanggalKegiatan: changeDateFormat(
+        data.PrestasiDataTable?.tanggalKegiatan
+      ),
+      penyelenggara: data.PrestasiDataTable?.penyelenggara || "-",
+      orkem: data.PrestasiDataTable?.orkem.name || "-",
+      tingkatKejuaraan: data.PrestasiDataTable?.tingkatKejuaraan.name || "-",
+      tingkatPrestasi: data.PrestasiDataTable?.tingkatPrestasi.name || "-",
+      dosen: data.dosen.name || "-",
+      piagamPenghargaan:
+        (
+          data.PrestasiDataTable?.lampiran
+            .piagamPenghargaan as PrismaJson.FileResponse
+        )?.secure_url || "-",
+      fotoPenyerahanPiala:
+        (
+          data.PrestasiDataTable?.lampiran
+            .fotoPenyerahanPiala as PrismaJson.FileResponse
+        )?.secure_url || "-",
+      undanganKejuaraan:
+        (
+          data.PrestasiDataTable?.lampiran
+            .undanganKejuaraan as PrismaJson.FileResponse
+        )?.secure_url || "-",
+      keterangan: data.keterangan || "-",
+      status: data.PrestasiDataTable?.status || "-",
+    }));
+
+    return transformedPrestasiData;
+  } catch (error) {
+    return error;
+  }
+});
+
+export default getAllChampionshipHandler;
