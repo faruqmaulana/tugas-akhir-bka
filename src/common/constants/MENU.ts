0e6a698050f986c2d9944ba9ed6e442b247a9c82
faruqmaulana <faruqchodot21@gmail.com>
2023-11-01 19:02:05 +0700

    feat: move sk rektor to master data

--- src/common/constants/MENU.ts
+++ src/common/constants/MENU.ts
@@ -97,6 +97,12 @@ const LIST_MENU: MenuItemType[] = [
         url: "/master-data/tingkat-prestasi",
         module: "check-point",
       },
+      {
+        id: 7,
+        title: "SK Rektor",
+        module: "cabang-sample-data",
+        url: "/module/sk-rektor",
+      },
     ],
   },
   {
